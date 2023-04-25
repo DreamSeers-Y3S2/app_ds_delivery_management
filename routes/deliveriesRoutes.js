1d1a8f6771e8b1fb9eff876c43c1078f8205119d
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2023-04-26 01:00:46 +0530

    updated delivery routes

--- routes/deliveriesRoutes.js
+++ routes/deliveriesRoutes.js
@@ -8,9 +8,9 @@ const {
 
 const router = express.Router();
 
-router.route("/customer/delivery/create").post(createDelivery);
-router.route("/order/delivery/get/:id").get(getDeliveriesForEachOrder);
+router.route("/create").post(createDelivery);
+router.route("/order/get/:id").get(getDeliveriesForEachOrder);
 
-router.route("/customer/delivery/get/:id").get(getDeliveryById).put(updateDelivery);
+router.route("/get/:id").get(getDeliveryById).put(updateDelivery);
 
 module.exports = router;
