bcb3faf0d60959871c6ec87c26ca8dd2afa6816e
Charith Lakshan <it20606374@my.sliit.lk>
2023-04-26 23:05:33 +0530

    added comments to delivery routes

--- routes/deliveriesRoutes.js
+++ routes/deliveriesRoutes.js
@@ -9,8 +9,11 @@ const {
 
 const router = express.Router();
 
+//delivery routes  for customer 
 router.route("/create").post(createDelivery);
 router.route("/all").get(getDeliveries);
+
+//delivery routes  for admin
 router.route("/customer/all/:id").get(getDeliveriesForEachCustomer);
 router.route("/get/:id").get(getDeliveryById).put(updateDelivery);
 
