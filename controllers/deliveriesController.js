8b5e1157883977d3d1d95dd8f26ddc1e35021448
Charith Lakshan <it20606374@my.sliit.lk>
2023-04-26 23:05:18 +0530

    added comments to delivery controller

--- controllers/deliveriesController.js
+++ controllers/deliveriesController.js
@@ -3,6 +3,7 @@ const Customer = require("../models/customerModel")
 const Order = require("../models/orderModel");
 const asyncHandler = require("express-async-handler");
 
+// create a delivery according to  order id
 const createDelivery = asyncHandler(async (req, res) => {
 	const {
 		order,
@@ -54,18 +55,19 @@ const createDelivery = asyncHandler(async (req, res) => {
 		console.log(req.body);
 	}
 });
-
+// get  delivery for each single customer
 const getDeliveriesForEachCustomer = asyncHandler(async (req, res) => {
 	const customer = await Customer.findById(req.params.id);
 	const deliveries = await Delivery.find({ customer: customer._id });
 	res.json(deliveries);
 });
-
+// get all of deliveries
 const getDeliveries = asyncHandler(async (req, res) => {
 	const deliveries = await Delivery.find();
 	res.json(deliveries);
 });
 
+// get deliveries according to their id
 const getDeliveryById = asyncHandler(async (req, res) => {
 	const delivery = await Delivery.findById(req.params.id);
 	console.log(delivery);
@@ -76,6 +78,7 @@ const getDeliveryById = asyncHandler(async (req, res) => {
 	}
 });
 
+// update delivery status 
 const updateDelivery = asyncHandler(async (req, res) => {
 	const {
 		status,
